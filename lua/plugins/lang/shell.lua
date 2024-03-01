68be546dec7ae7aedeb045883c7c45a70f58feba
Jan Bücker <j.buecker@shopware.com>
2024-03-01 13:20:57 +0100

    Fix shell linting

--- lua/plugins/lang/shell.lua
+++ lua/plugins/lang/shell.lua
@@ -8,6 +8,15 @@ return {
         end,
     },
     {
+        "williamboman/mason.nvim",
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, {
+                "shellcheck",
+                "shfmt",
+            })
+        end,
+    },
+    {
         "stevearc/conform.nvim",
         opts = function(_, opts)
             if type(opts.formatters_by_ft) == "table" then
@@ -21,7 +30,7 @@ return {
         "mfussenegger/nvim-lint",
         opts = function(_, opts)
             opts.linters_by_ft = vim.tbl_deep_extend("force", opts.linters_by_ft, {
-                sh = { "shfmt" },
+                sh = { "shellcheck" },
             })
         end,
     },
