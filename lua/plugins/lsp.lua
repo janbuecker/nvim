fd23b9301dd0d0e8b6df20f8470f9c9edba53cb3
Jan Bücker <j.buecker@shopware.com>
2024-03-12 08:36:55 +0100

    Use diagnostic icons 0.10

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,12 @@
 local Util = require("util")
 
+local icons = {
+    Error = " ",
+    Warn = " ",
+    Hint = " ",
+    Info = " ",
+}
+
 return {
     {
         "neovim/nvim-lspconfig",
@@ -20,62 +27,45 @@ return {
                 virtual_text = {
                     spacing = 4,
                     source = "if_many",
-                    prefix = "●",
-                    -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
-                    -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
-                    -- prefix = "icons",
+                    prefix = function(diagnostic)
+                        for d, icon in pairs(icons) do
+                            if diagnostic.severity == vim.diagnostic.severity[d:upper()] then
+                                return icon
+                            end
+                        end
+                    end,
                 },
                 severity_sort = true,
+                signs = {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = icons.Error,
+                        [vim.diagnostic.severity.WARN] = icons.Warn,
+                        [vim.diagnostic.severity.HINT] = icons.Hint,
+                        [vim.diagnostic.severity.INFO] = icons.Info,
+                    },
+                },
             },
-            -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
-            -- Be aware that you also will need to properly configure your LSP server to
-            -- provide the inlay hints.
-            inlay_hints = {
-                enabled = false,
-            },
-            -- add any global capabilities here
             capabilities = {},
-            -- Automatically format on save
-            autoformat = true,
-            format = {
-                formatting_options = nil,
-                timeout_ms = nil,
-            },
             servers = {},
             setup = {},
         },
 
         config = function(_, opts)
-            -- LSP settings.
-            --  This function gets run when an LSP connects to a particular buffer.
+            vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
+
             Util.on_attach(function(client, bufnr)
                 require("plugins.lsp.keymaps").on_attach(client, bufnr)
             end)
 
-            local register_capability = vim.lsp.handlers["client/registerCapability"]
-
-            vim.lsp.handlers["client/registerCapability"] = function(err, res, ctx)
-                local ret = register_capability(err, res, ctx)
-                local client_id = ctx.client_id
-                ---@type lsp.Client
-                local client = vim.lsp.get_client_by_id(client_id)
-                local buffer = vim.api.nvim_get_current_buf()
-                require("plugins.lsp.keymaps").on_attach(client, buffer)
-                return ret
-            end
-
-            vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
-
-            local servers = opts.servers
-            local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
             local capabilities = vim.tbl_deep_extend(
                 "force",
                 {},
                 vim.lsp.protocol.make_client_capabilities(),
-                has_cmp and cmp_nvim_lsp.default_capabilities() or {},
+                require("cmp_nvim_lsp").default_capabilities(),
                 opts.capabilities or {}
             )
 
+            local servers = opts.servers
             local function setup(server)
                 local server_opts = vim.tbl_deep_extend("force", {
                     capabilities = vim.deepcopy(capabilities),
@@ -89,18 +79,11 @@ return {
                 require("lspconfig")[server].setup(server_opts)
             end
 
-            -- get all the servers that are available thourgh mason-lspconfig
-            local have_mason, mlsp = pcall(require, "mason-lspconfig")
-            local all_mslp_servers = {}
-            if have_mason then
-                all_mslp_servers = vim.tbl_keys(require("mason-lspconfig.mappings.server").lspconfig_to_package)
-            end
-
+            local all_mslp_servers = vim.tbl_keys(require("mason-lspconfig.mappings.server").lspconfig_to_package)
             local ensure_installed = {} ---@type string[]
             for server, server_opts in pairs(servers) do
                 if server_opts then
                     server_opts = server_opts == true and {} or server_opts
-                    -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
                     if server_opts.mason == false or not vim.tbl_contains(all_mslp_servers, server) then
                         setup(server)
                     else
@@ -109,9 +92,7 @@ return {
                 end
             end
 
-            if have_mason then
-                mlsp.setup({ ensure_installed = ensure_installed, handlers = { setup } })
-            end
+            require("mason-lspconfig").setup({ ensure_installed = ensure_installed, handlers = { setup } })
         end,
     },
 
