45d15f971d4846830180d619c4a75b06e529049e
Jan Bücker <j.buecker@shopware.com>
2024-05-21 10:03:01 +0200

    Use hcl formatter instead of terragrunt hclfmt

--- lua/plugins/lang/terraform.lua
+++ lua/plugins/lang/terraform.lua
@@ -13,6 +13,7 @@ return {
         opts = function(_, opts)
             vim.list_extend(opts.ensure_installed, {
                 "terraform-ls",
+                "hclfmt",
             })
         end,
     },
@@ -30,8 +31,8 @@ return {
             opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft, {
                 terraform = { "terraform_fmt" },
                 tf = { "terraform_fmt" },
-                hcl = { "terragrunt_hclfmt" },
                 ["terraform-vars"] = { "terraform_fmt" },
+                hcl = { "hcl" },
             })
         end,
     },
