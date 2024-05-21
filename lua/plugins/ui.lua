8e5e68e9b45a2c40c8d0b7180356950cf08f9b5d
Jan Bücker <j.buecker@shopware.com>
2024-05-21 10:05:13 +0200

    Remove noice and lualine

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,57 +1,5 @@
 return {
     {
-        "folke/noice.nvim",
-        event = "VeryLazy",
-        dependencies = {
-            "MunifTanjim/nui.nvim",
-            "rcarriga/nvim-notify",
-        },
-        opts = {
-            lsp = {
-                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true,
-                },
-            },
-            -- you can enable a preset for easier configuration
-            presets = {
-                bottom_search = true, -- use a classic bottom cmdline for search
-                command_palette = true, -- position the cmdline and popupmenu together
-                long_message_to_split = true, -- long messages will be sent to a split
-                inc_rename = false, -- enables an input dialog for inc-rename.nvim
-                -- lsp_doc_border = false, -- add a border to hover docs and signature help
-            },
-
-            routes = {
-                {
-                    filter = {
-                        event = "msg_show",
-                        any = {
-                            { find = "%d+L, %d+B" },
-                            { find = "; after #%d+" },
-                            { find = "; before #%d+" },
-                        },
-                    },
-                    view = "mini",
-                },
-            },
-        },
-    },
-
-    {
-        "nvim-lualine/lualine.nvim",
-        event = "VeryLazy",
-        opts = {
-            options = {
-                globalstatus = true,
-                component_separators = "|",
-            },
-        },
-    },
-
-    {
         "lewis6991/gitsigns.nvim",
         event = "BufReadPre",
         opts = {
