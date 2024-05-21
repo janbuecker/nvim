aa4630aa8481624ad0fb05d1d59f92602409722b
Jan Bücker <j.buecker@shopware.com>
2024-05-21 10:06:13 +0200

    Replace nvimtree with mini.files

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -11,6 +11,10 @@ return {
         })
     end,
 
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+
     config = function()
         require("mini.move").setup()
         require("mini.bracketed").setup()
@@ -18,8 +22,18 @@ return {
         require("mini.trailspace").setup()
         require("mini.bufremove").setup()
         require("mini.cursorword").setup()
+        require("mini.statusline").setup()
+        require("mini.notify").setup()
+        require("mini.splitjoin").setup()
         require("mini.misc").setup({ make_global = { "setup_auto_root" } })
         require("mini.misc").setup_auto_root()
+
+        require("mini.files").setup({
+            mappings = {
+                go_in_plus = "<Right>",
+                go_out_plus = "<Left>",
+            },
+        })
 
         require("mini.indentscope").setup({
             -- symbol = "▏",
@@ -37,14 +51,63 @@ return {
                         or vim.bo.commentstring
                 end,
             },
-            mappings = {
-                comment_line = "<leader>/",
-                comment_visual = "<leader>/",
-            },
+        })
+
+        local MiniFiles = require("mini.files")
+        local map_split = function(buf_id, lhs, direction)
+            local rhs = function()
+                -- Make new window and set it as target
+                local new_target_window
+                vim.api.nvim_win_call(MiniFiles.get_target_window(), function()
+                    vim.cmd(direction .. " split")
+                    new_target_window = vim.api.nvim_get_current_win()
+                end)
+
+                MiniFiles.set_target_window(new_target_window)
+            end
+
+            vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = "Split " .. direction })
+        end
+
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "MiniFilesBufferCreate",
+            callback = function(args)
+                local buf_id = args.data.buf_id
+
+                -- Open in split
+                map_split(buf_id, "gs", "belowright horizontal")
+                map_split(buf_id, "gv", "belowright vertical")
+
+                -- Grep in folder
+                vim.keymap.set("n", "gg", function()
+                    local cur_entry_path = MiniFiles.get_fs_entry().path
+                    local basedir = vim.fs.dirname(cur_entry_path)
+                    MiniFiles.close()
+                    require("fzf-lua").live_grep({ cwd = basedir })
+                end, { buffer = buf_id, desc = "Grep in directory" })
+
+                -- Find in folder
+                vim.keymap.set("n", "f", function()
+                    local cur_entry_path = MiniFiles.get_fs_entry().path
+                    local basedir = vim.fs.dirname(cur_entry_path)
+                    MiniFiles.close()
+                    require("fzf-lua").files({ cwd = basedir })
+                end, { buffer = buf_id, desc = "Grep in directory" })
+
+                -- Find in project
+                vim.keymap.set("n", "gf", function()
+                    MiniFiles.close()
+                    require("fzf-lua").files()
+                end, { buffer = buf_id, desc = "Grep in directory" })
+            end,
         })
     end,
     -- stylua: ignore
     keys = {
         { "<leader>X", function() require("mini.bufremove").delete(0, false) end, desc = "Delete Buffer", },
+        { "<leader>e", function()
+            local MiniFiles = require("mini.files")
+            if not MiniFiles.close() then MiniFiles.open(vim.api.nvim_buf_get_name(0)) end
+        end, desc = "Open file explorer"}
     },
 }
