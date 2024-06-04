ccb856d703823a12dee0bd51c61456bfc6b9b923
Jan Bücker <j.buecker@shopware.com>
2024-06-04 12:25:10 +0200

    Remove deprecated config handling

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ return {
     {
         "f-person/auto-dark-mode.nvim",
         lazy = false,
-        config = {
+        opts = {
             update_interval = 1000,
             set_dark_mode = function()
                 vim.api.nvim_set_option("background", "dark")
