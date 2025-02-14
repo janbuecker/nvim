f90834c1728408b2c9b8bdc6a027f7515fba232f
Jan Bücker <j.buecker@shopware.com>
2025-02-14 07:17:18 +0100

    Disable incremental selection in TS

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,26 +5,17 @@ local M = {
     build = ":TSUpdate",
     cmd = { "TSUpdateSync" },
     dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
+        { "nvim-treesitter/nvim-treesitter-textobjects" },
         {
             "folke/ts-comments.nvim",
             opts = {},
-            event = "InsertEnter",
+            event = "VeryLazy",
         },
     },
     opts = {
         ensure_installed = {},
         highlight = { enable = true },
         indent = { enable = true },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<c-space>",
-                node_incremental = "<c-space>",
-                scope_incremental = "<c-s>",
-                node_decremental = "<c-backspace>",
-            },
-        },
     },
     config = function(_, opts)
         if type(opts.ensure_installed) == "table" then
